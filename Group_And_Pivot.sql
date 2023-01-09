7788c28df826ae814987f926d87cf23e8bb12adb
Filip Copija <filip.copija1@gmail.com>
2023-01-09 15:48:17 +0100

    Update Group_And_Pivot.sql

--- Group_And_Pivot.sql
+++ Group_And_Pivot.sql
@@ -120,3 +120,142 @@ JOIN modules m ON e.employee_id = m.lecturer_id
 LEFT JOIN student_grades sg ON sg.module_id = m.module_id
 GROUP BY e.surname, e.first_name, e.employee_id
 ORDER BY avg_grade DESC
+
+-- 4a.13a
+/* Nazwy wykładów oraz kwotę, jaką uczelnia musi przygotować na wypłaty pracownikom prowadzącym wykłady 
+ze Statistics i Economics (osobno). Jeśli jest wiele wykładów o nazwie Statistics lub Economics, suma 
+dla nich ma być obliczona łącznie. Zapytanie ma więc zwrócić dwa rekordy (jeden dla wykładów ze 
+Statistics, drugi dla Economics).
+
+Kwotę za jeden wykład należy obliczyć jako iloczyn stawki godzinowej prowadzącego wykładowcy oraz 
+liczby godzin przeznaczonych na wykład. */
+
+SELECT m.module_name, (m.no_of_hours * ap.overtime_rate) as result
+FROM modules m 
+JOIN lecturers l ON l.lecturer_id = m.lecturer_id
+JOIN acad_positions ap ON ap.acad_position = l.acad_position 
+GROUP BY m.module_name, (m.no_of_hours * ap.overtime_rate)
+HAVING module_name = 'Economics' OR module_name = 'Statistics'
+
+-- dla wykładu statistics lecturer o id = 30 nie ma przypisanego acad_position
+
+-- 4a.13b
+/* Sumaryczną kwotę, jaką uczelnia musi wypłacić wykładowcom łącznie z tytułu prowadzenia 
+przez nich wykładów. */
+
+SELECT SUM(m.no_of_hours * ap.overtime_rate) as result
+FROM modules m 
+JOIN lecturers l ON l.lecturer_id = m.lecturer_id
+JOIN acad_positions ap ON ap.acad_position = l.acad_position 
+
+-- 4a.13c
+/* Kwotę, jaką uczelnia musi przygotować na wypłaty z tytułu prowadzenia wykładów, 
+którym nie jest przypisany żaden wykładowca, przy założeniu, że za godzinę takiego 
+wykładu należy zapłacić średnią z pola overtime_rate w tabeli acad_positions. */
+
+SELECT 
+(
+	SELECT SUM(m.no_of_hours)
+	FROM modules m
+	LEFT JOIN lecturers l ON m.lecturer_id = l.lecturer_id
+	WHERE m.lecturer_id IS NULL 
+)
+*
+(
+	SELECT AVG(overtime_rate)
+	FROM acad_positions
+)
+
+-- 4a.13d
+/* Kwotę, jaką uczelnia musi przygotować na wypłaty z tytułu prowadzenia wszystkich 
+wykładów, za które nie można ustalić stawki godzinowej. Przyjmij założenie, że za 
+godzinę takiego wykładu należy zapłacić maksymalną wartość z pola overtime_rate w 
+tabeli acad_positions. */
+
+SELECT 
+(
+	SELECT SUM(m.no_of_hours)
+	FROM modules m
+	LEFT JOIN lecturers l ON m.lecturer_id = l.lecturer_id
+	WHERE m.lecturer_id IS NULL 
+		OR l.acad_position IS NULL
+)
+*
+(
+	SELECT MAX(overtime_rate)
+	FROM acad_positions
+)
+
+-- 4a.14 (?)
+/* Nazwiska i imiona wykładowców wraz z sumaryczną liczbą godzin wykładów prowadzonych 
+przez każdego z nich z osobna ale tylko w przypadku, gdy suma godzin prowadzonych wykładów
+jest większa od 30. Kolumna zwracająca liczbę godzin ma mieć nazwę no_of_hours. Dane 
+posortowane malejąco według liczby godzin. */
+
+SELECT e.surname, e.first_name, SUM(m.no_of_hours) AS no_of_hours
+FROM modules m
+JOIN lecturers l ON l.lecturer_id = m.lecturer_id
+JOIN employees e ON e.employee_id = l.lecturer_id
+GROUP BY e.surname, e.first_name
+HAVING SUM(m.no_of_hours) > 30
+ORDER BY no_of_hours DESC
+
+-- 4a.15
+/* Nazwy wszystkich grup oraz liczbę studentów zapisanych do każdej grupy
+(kolumna ma mieć nazwę no_of_students). Dane posortowane rosnąco według 
+liczby studentów a następnie numeru grupy. */
+
+SELECT g.group_no, COUNT(student_id) AS no_of_students
+FROM groups g
+LEFT JOIN students s ON g.group_no = s.group_no
+GROUP BY g.group_no
+ORDER BY COUNT(student_id), g.group_no
+
+-- 4a.16
+/* Nazwy wszystkich wykładów, których nazwa zaczyna się literą A oraz średnią ocen ze 
+wszystkich tych wykładów osobno (jeśli jest wiele takich wykładów, to średnia ma być 
+obliczona dla każdego z nich oddzielnie). Jeśli z danego wykładu nie ma żadnej oceny, 
+także powinien on pojawić się na liście. Kolumna ma mieć nazwę average. */
+
+SELECT m.module_name, AVG(sg.grade) AS average
+FROM modules m
+LEFT JOIN student_grades sg ON m.module_id = sg.module_id
+GROUP BY m.module_name
+HAVING m.module_name LIKE 'A%'
+
+-- 4a.17
+/* Nazwy grup, do których jest zapisanych co najmniej dwóch studentów, liczba studentów 
+zapisanych do tych grup (kolumna ma mieć nazwę no_of_students) oraz średnie ocen dla 
+każdej grupy (kolumna ma mieć nazwę average_grade). Dane posortowane malejąco według średniej. */
+
+SELECT group_no, COUNT(s.student_id) AS no_of_students, AVG(sg.grade) AS average_grade
+FROM students s
+JOIN student_grades sg ON sg.student_id = s.student_id
+GROUP BY group_no
+HAVING COUNT(s.student_id) >= 2
+ORDER BY  average_grade DESC
+
+-- 4a.18
+/* Nazwy tych katedr (department), w których pracuje co najmniej 2 doktorów (doctor) 
+wraz z liczbą doktorów pracujących w tych katedrach (ta ostatnia kolumna ma mieć 
+nazwę no_of_doctors). Dane posortowane malejąco według liczby doktorów i rosnąco
+według nazw katedr. */
+
+SELECT department, COUNT(acad_position) AS no_of_doctors
+FROM lecturers
+WHERE acad_position = 'doctor'
+GROUP BY department, acad_position
+HAVING COUNT(acad_position) >= 2
+ORDER BY no_of_doctors DESC, department ASC
+
+-- 4a.19 (?)
+/* Identyfikatory, nazwy wykładów oraz nazwy katedr odpowiedzialnych za prowadzenie wykładów, 
+dla których nie można ustalić kwoty, jaką trzeba zapłacić za ich przeprowadzenie wraz z nazwiskiem
+i imieniem dowolnego spośród pracowników tych katedr. Dane posortowane według module_id. */
+
+SELECT module_id, module_name, m.department, CONCAT(e.surname,' ',e.first_name) employee
+FROM modules m
+LEFT JOIN lecturers l ON m.lecturer_id = l.lecturer_id
+LEFT JOIN employees e ON l.lecturer_id = e.employee_id
+WHERE m.lecturer_id IS NULL
+ORDER BY module_id
